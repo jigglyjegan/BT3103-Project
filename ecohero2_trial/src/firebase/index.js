c6bac410c3514f68595c4c1c60c261f8ae842885
cheekang123 <ngcheekang@live.com>
2022-03-12 23:14:09 +0800

    firebase setup cont

---
+++ ecohero2_trial/src/firebase/index.js
@@ -0,0 +1,14 @@
+import { intializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDbcRBFZZ3tcLzJObSgTnjQfIrn3AwCxH8",
+    authDomain: "eco-hero-15b21.firebaseapp.com",
+    projectId: "eco-hero-15b21",
+    storageBucket: "eco-hero-15b21.appspot.com",
+    messagingSenderId: "185271524094",
+    appId: "1:185271524094:web:c3586f182e4f89dfb2e3af",
+    measurementId: "G-TKLMEYXXK8"
+  };
+
+
+const app = intializeApp(firebaseConfig);
