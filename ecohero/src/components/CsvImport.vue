114d7c89922fdcbc7779d3cf9a85b11d8aafea87
ryanklk <ryankoh0429@yahoo.com>
2022-03-28 11:17:22 +0800

    Testing out the CsvImport + UploadPage page

---
+++ ecohero/src/components/CsvImport.vue
@@ -0,0 +1,50 @@
+<template>
+<div id="import">
+    <!-- <img alt="Vue logo" src="./assets/logo.png" width="25%" /> -->
+    <vue-csv-import
+      v-model="csv"
+      :autoMatchFields="true"
+      :autoMatchIgnoreCase="true"
+      :map-fields="[
+        'ID',
+        'Date',
+        'Department',
+        'Carbon (metric tonne)',
+        'Electricity (kwh)',
+        'Water (m3)',
+      ]"
+    >
+    </vue-csv-import>
+    <div style="padding-top: 50px">
+      <p>Results</p>
+      {{ csv }}
+    </div>
+  </div>
+</template>
+
+<script>
+import { VueCsvImport } from "vue-csv-import";
+
+export default {
+  name: "CsvImport",
+  components: {
+    VueCsvImport,
+  },
+  data() {
+    return {
+      csv: null,
+    };
+  },
+};
+</script>
+
+<style>
+#CsvImport {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- ecohero/src/router/index.js
+++ ecohero/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import About from "@/views/About.vue";
 import ContactUs from "@/views/ContactUs.vue";
+import UploadPage from "@/views/UploadPage.vue";
 const routes = [
   {
     path: "/",
@@ -19,6 +20,12 @@ const routes = [
     path: "/contactUs",
     name: "ContactUs",
     component: ContactUs,
+  },
+
+  {
+    path: "/UploadPage",
+    name: "UploadPage",
+    component: UploadPage,
   },
 ];
 const router = createRouter({
