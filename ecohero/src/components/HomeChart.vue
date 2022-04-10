fa451dc88e7f7d92ae7ca83a6be04274cbb93e83
Marklim99 <77767460+Marklim99@users.noreply.github.com>
2022-04-10 20:47:32 +0800

    Changed Home chart and ChangeLimit to dynamic

--- ecohero/src/components/HomeChart.vue
+++ ecohero/src/components/HomeChart.vue
@@ -59,7 +59,7 @@ export default {
                 monthName = String(month)
             }
             console.log("month:" + monthName)
-            const year = date.getFullYear()
+            const year = 2020 //date.getFullYear()
 
             var userThreshold = await getDoc(doc(db, "/limits", uid))
             if (userThreshold.exists()){
@@ -136,73 +136,8 @@ export default {
                 alert("you have no data!")
             }
 
-
-
-
-
-            /*var dataRef1 = uid + "Finance" + year;
-            var dataRef2 = uid + "IT" + year 
-            var dataRef3 = uid + "Logistics" + year
-
-            var electricityFinance = await getDoc(doc(db, "/elecUsageMthly",dataRef1))
-            var electricityIT = await getDoc(doc(db, "/elecUsageMthly",dataRef2))
-            var electricityLogistics = await getDoc(doc(db, "/elecUsageMthly",dataRef3))
-            var electricityFinanceValue = electricityFinance.data()
-            console.log(electricityFinanceValue)
-            var electricityITValue = electricityIT.data()
-            console.log(electricityITValue)
-            var electrictyLogisticsValue = electricityLogistics.data()
-            console.log(electrictyLogisticsValue)
-            var thisMonthElectric = 
-            electricityFinanceValue[monthName] + electricityITValue[monthName] + electrictyLogisticsValue[monthName]
-            console.log(thisMonthElectric)
-            if (electricityThresholdValue - thisMonthElectric <= 0 ){
-                this.dataElectric = {'Used': thisMonthElectric, 'Left': 0}
-            }
-            else{
-                this.dataElectric = {'Used': thisMonthElectric, 'Left': electricityThresholdValue - thisMonthElectric}
-            }
-
-            var waterFinance = await getDoc(doc(db, "/waterUsageMthly",dataRef1))
-            var waterIT = await getDoc(doc(db, "/waterUsageMthly",dataRef2))
-            var waterLogistics = await getDoc(doc(db, "/waterUsageMthly",dataRef3))
-            var waterFinanceValue = waterFinance.data()
-            console.log(waterFinanceValue)
-            var waterITValue = waterIT.data()
-            console.log(waterITValue)
-            var waterLogisticsValue = waterLogistics.data()
-            console.log(waterLogisticsValue)
-            var thisMonthWater = waterFinanceValue[monthName] + waterITValue[monthName] + waterLogisticsValue[monthName]
-            console.log(thisMonthWater)
-            if (waterThresholdValue - thisMonthWater <= 0 ){
-                this.dataWater = {'Used': thisMonthWater, 'Left': 0}
-            }
-            else{
-                this.dataWater = {'Used': thisMonthWater, 'Left': waterThresholdValue - thisMonthWater}
-            }
-
-
-           var carbonFinance = await getDoc(doc(db, "/carbonUsageMthly",dataRef1))
-            var carbonIT = await getDoc(doc(db, "/carbonUsageMthly",dataRef2))
-            var carbonLogistics = await getDoc(doc(db, "/carbonUsageMthly",dataRef3))
-            var carbonFinanceValue = carbonFinance.data()
-            console.log(carbonFinanceValue)
-            var carbonITValue = carbonIT.data()
-            console.log(carbonITValue)
-            var carbonLogisticsValue = carbonLogistics.data()
-            console.log(carbonLogisticsValue)
-            var thisMonthCarbon = carbonFinanceValue[monthName] + carbonITValue[monthName] + carbonLogisticsValue[monthName]
-            console.log(thisMonthCarbon)
-           if (carbonThresholdValue - thisMonthCarbon <= 0 ){
-                this.dataCarbon = {'Used': thisMonthCarbon, 'Left': 0}
-            }
-            else{
-                this.dataCarbon = {'Used': thisMonthCarbon, 'Left': carbonThresholdValue - thisMonthCarbon}
-            }*/
-
         }
     }
-
 
 
 }
--- ecohero/src/views/ChangeLimit.vue
+++ ecohero/src/views/ChangeLimit.vue
@@ -190,66 +190,7 @@ export default {
             } else{
                 alert("you have no data!")
             }  
-      
-
-
-      /*var dataRef1 = uid + "Finance" + year;
-      var dataRef2 = uid + "IT" + year 
-      var dataRef3 = uid + "Logistics" + year
-
-      var electricityFinance = await getDoc(doc(db, "/elecUsageMthly",dataRef1))
-      var electricityIT = await getDoc(doc(db, "/elecUsageMthly",dataRef2))
-      var electricityLogistics = await getDoc(doc(db, "/elecUsageMthly",dataRef3))
-      var electricityFinanceValue = electricityFinance.data()
-      console.log(electricityFinanceValue)
-      var electricityITValue = electricityIT.data()
-      console.log(electricityITValue)
-      var electrictyLogisticsValue = electricityLogistics.data()
-      console.log(electrictyLogisticsValue)
-      var thisMonthElectric = 
-      electricityFinanceValue[String(monthName)] + electricityITValue[String(monthName)] + electrictyLogisticsValue[String(monthName)]
-      console.log(thisMonthElectric)
-
-      var waterFinance = await getDoc(doc(db, "/waterUsageMthly",dataRef1))
-      var waterIT = await getDoc(doc(db, "/waterUsageMthly",dataRef2))
-      var waterLogistics = await getDoc(doc(db, "/waterUsageMthly",dataRef3))
-      var waterFinanceValue = waterFinance.data()
-      console.log(waterFinanceValue)
-      var waterITValue = waterIT.data()
-      console.log(waterITValue)
-      var waterLogisticsValue = waterLogistics.data()
-      console.log(waterLogisticsValue)
-      var thisMonthWater = waterFinanceValue[String(monthName)] + waterITValue[String(monthName)] + waterLogisticsValue[String(monthName)]
-      console.log(thisMonthWater)
-
-      var carbonFinance = await getDoc(doc(db, "/carbonUsageMthly",dataRef1))
-      var carbonIT = await getDoc(doc(db, "/carbonUsageMthly",dataRef2))
-      var carbonLogistics = await getDoc(doc(db, "/carbonUsageMthly",dataRef3))
-      var carbonFinanceValue = carbonFinance.data()
-      console.log(carbonFinanceValue)
-      var carbonITValue = carbonIT.data()
-      console.log(carbonITValue)
-      var carbonLogisticsValue = carbonLogistics.data()
-      console.log(carbonLogisticsValue)
-      var thisMonthCarbon = carbonFinanceValue[String(monthName)] + carbonITValue[String(monthName)] + carbonLogisticsValue[String(monthName)]
-      console.log(thisMonthCarbon)
-
-      if(waterThresholdValue < thisMonthWater){
-        alert("You have exceeded your water usage limit")
-        notExceededAny = false
-      }
-      if(carbonThresholdValue < thisMonthCarbon){
-        alert("You have exceeded your carbon emissions limit")
-        notExceededAny = false
-      }
-      if(electricityThresholdValue < thisMonthElectric){
-        alert("You have exceeded your electricity usage limit")
-        notExceededAny = false
-      }
-
-      if(notExceededAny){
-        alert("No metrics exceeded")
-      }*/
+    
 
     }
 
